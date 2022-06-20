ad7cb54b02145d6d706d61636d59b14c296a3abe
kkakoz <kkakoz@163.com>
2022-06-20 17:20:26 +0800

    fist commit

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+	"log"
+	"path/filepath"
+)
+
+func main() {
+
+	var kubeconfig *string
+
+	// home是家目录，如果能取得家目录的值，就可以用来做默认值
+	if home := homedir.HomeDir(); home != "" {
+		// 如果输入了kubeconfig参数，该参数的值就是kubeconfig文件的绝对路径，
+		// 如果没有输入kubeconfig参数，就用默认路径~/.kube/config
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		// 如果取不到当前用户的家目录，就没办法设置kubeconfig的默认目录了，只能从入参中取
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+
+	flag.Parse()
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		log.Println(err)
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Fatalln(err)
+	} else {
+		fmt.Println("connect k8s success")
+	}
+
+	deployments := clientset.AppsV1().Deployments("default")
+	fmt.Println(deployments)
+}
